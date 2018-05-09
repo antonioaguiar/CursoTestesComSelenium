a08642fa553c438b092f323c355d9bb9ca30dce5
Antonio Aguiar <antonioaguiar@gmail.com>
2018-05-09 09:00:28 -0400

    detecção do SO e busca do webdriver na pasta resources do pacote.

--- selenium/src/main/java/br/eti/aaguiar/DriverSingleton.java
+++ selenium/src/main/java/br/eti/aaguiar/DriverSingleton.java
@@ -1,5 +1,7 @@
 package br.eti.aaguiar;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,18 +11,30 @@ public class DriverSingleton {
 	public static String CHROME = "chrome"; 
 	public static String FIREFOX = "firefox";
 	private static WebDriver driver;
-
+	private static String SO = System.getProperty("os.name").toLowerCase();
+	private static String PATHDRIVER =  System.getProperty("user.dir");
+	
 	public static WebDriver getDriver(String browser) {
-				
+		String drivername = "";
+		String sep = File.separator;
+		PATHDRIVER += sep+"src"+sep+"main"+sep+"resources"+sep;
 		
 		if (FIREFOX.equals(browser)) {
-			String caminho = "/home/aguiar/Downloads/drivers/geckodriver";
+			drivername = "geckodriver";
+			if (SO.indexOf("win") >= 0){
+				drivername +=".exe";
+			}
+			String caminho = PATHDRIVER+drivername;
 			System.setProperty("webdriver.gecko.driver", caminho);
 			driver = new FirefoxDriver();
 		}
 		
 		if (CHROME.equals(browser)) {
-			String caminho = "/home/aguiar/Downloads/drivers/chromedriver";
+			drivername = "chromedriver";
+			if (SO.indexOf("win") >= 0){
+				drivername +=".exe";
+			}
+			String caminho = PATHDRIVER+drivername;
 		    System.setProperty("webdriver.chrome.driver", caminho);
 			driver = new ChromeDriver();
 		}
--- selenium/src/main/java/br/eti/aaguiar/ElementosBasicos.java
+++ selenium/src/main/java/br/eti/aaguiar/ElementosBasicos.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class ElementosBasicos
